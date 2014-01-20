efac56f83eaac945a7ce61ea6ca7966fa80b2a15
Michael Williams <dinosaur@riseup.net>
2014-01-20 15:44:13 -0800

    add popover text to nav, add screen reader text to nav

--- src/render/scripts/index.js
+++ src/render/scripts/index.js
@@ -1,5 +1,9 @@
-var $ = window.jQuery = require('jquery/dist/jquery')(window);
+var $ = jQuery = require('jquery/dist/jquery')(window);
+require('./vendor/jquery.easing');
+
+require('semantic/src/modules/popup');
 
 $(function () {
+  $('body > main > nav > ul.menu > li.item i.icon').popup();
   require('./deobfuscate')();
 });
