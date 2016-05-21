aa90013236510bbfe9e2b28010da32d7c128dfc8
Michael Williams <michael.williams@enspiral.com>
2016-05-21 20:05:04 +1200

    de-obfuscate without the jquery

--- src/deobfuscate.js
+++ src/deobfuscate.js
@@ -1,21 +1,43 @@
-var $ = require('jquery');
-
 module.exports = function () {
   /*
   event handler to deobfuscate obfuscated elements
   such as email addresses on mouseenter or click
   */
-  $('.obfuscated').on('mouseenter.deobfuscate click.deobfuscate', function (e) {
-    var el = $(this);
+  var obfuscated = document.querySelectorAll('.obfuscated')
 
-    if (el.attr('href')) {
-      // http://stackoverflow.com/questions/11047670/creating-a-jquery-object-from-a-big-html-string
-      $obj = $($.parseHTML('<div>' + el.attr('href') + '</div>'));
-      // http://stackoverflow.com/questions/3331449/jquery-remove-element-from-string
-      el.attr('href', $obj.find('.obfuscation').remove().end().text());
+  for (var i = 0; i < obfuscated.length; i++) {
+    addEvent(obfuscated.item(i))
+  }
+}
+
+function addEvent (element) {
+  // add event listeners
+  element.addEventListener('mouseenter', handleEvent)
+  element.addEventListener('click', handleEvent)
+
+  function handleEvent (ev) {
+    if (element.getAttribute('href')) {
+      var pseudoElement = document.createElement('div')
+      pseudoElement.innerHTML = element.getAttribute('href')
+      var href = removeObfuscation(pseudoElement).innerHTML
+      element.setAttribute('href', href)
     }
-    el.find('.obfuscation').remove();
-    el.removeClass('obfuscated');
-    el.off('.deobfuscate');
-  });
-};
+    // remove any obfuscation child elements
+    removeObfuscation(element)
+
+    // remove obfuscated class
+    element.classList.remove('obfuscated')
+  
+    // remove event listeners
+    element.removeEventListener('mouseenter', handleEvent)
+    element.removeEventListener('click', handleEvent)
+  }
+}
+
+function removeObfuscation (element) {
+  var obfuscation = element.querySelectorAll('.obfuscation')
+  for (var i = 0; i < obfuscation.length; i++) {
+    element.removeChild(obfuscation.item(i))
+  }
+  return element
+}
