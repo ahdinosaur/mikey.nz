93b5e97fce20851d6289b8a95b1adc64860330e6
Michael Williams <michael.williams@enspiral.com>
2023-01-10 13:32:31 +1300

    migrate redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const { readFile } = require('fs/promises')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return (await readFile('./redirects.txt', 'utf8'))
+      .trim().split('\n')
+      .map(redirect => redirect.split(' '))
+      .map(([source, destination]) => ({
+        source: `${source}/:path*`,
+        destination: `${destination}/:path*`,
+        permanent: false,
+      }))
+  }
 }
 
 module.exports = nextConfig
